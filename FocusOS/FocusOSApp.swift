63b0b9b1013228667977724ec36a1a173642c100
Sheldon <sheldonxie19@gmail.com>
2026-02-13 16:02:11 -0800

    Initial Commit

---
+++ FocusOS/FocusOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusOSApp.swift
+//  FocusOS
+//
+//  Created by Sheldon on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
